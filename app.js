9a0012579fe572799478ff2fcb2e3093ffb25485
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2022-09-07 01:04:31 +0530

    Added 12 hours fuctionality

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 setInterval(()=>{
+    // variables to use
     let hours = document.getElementById('hours');
     let minutes = document.getElementById('minutes');
     let seconds = document.getElementById('seconds');
     let ampm = document.getElementById('ampm');
+
+    let hh = document.getElementById('hh');
+    let mm = document.getElementById('mm');
+    let ss = document.getElementById('ss');
 
     let h = new Date().getHours();
     let m = new Date().getMinutes();
@@ -23,4 +28,8 @@ setInterval(()=>{
     minutes.innerHTML = m;
     seconds.innerHTML = s;
     ampm.innerHTML = am;
+
+    hh.style.strokeDashoffset = 440 - (440 * h) / 12;    // 12 hours clock
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60;    // 60 minutes
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60;    // 60 seconds
 });
